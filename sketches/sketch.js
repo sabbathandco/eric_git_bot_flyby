b7e26b16f0de415f2fb8e6413d1dbb9a9db913d4
sabbathandco <141058023+sabbathandco@users.noreply.github.com>
2023-11-20 20:51:27 -0500

    added p5

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,13 +1,16 @@
 const canvasSketch = require('canvas-sketch');
+const p5 = require("p5");
+new p5();
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  p5: true,
+  dimensions: [400, 400],
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+  return () => {
+    background(0);
+    circle(width / 2, height / 2, 50);
   };
 };
 
